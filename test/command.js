6f6d4c4b33560e1bdb565eb52f219e22e6f9dced
funkytek <aaron@wearefractal.com>
2018-03-02 09:26:57 -0800

    chore: update package; chore: mocha- ui -> exports

--- test/command.js
+++ test/command.js
@@ -5,50 +5,52 @@ const rimraf = require('rimraf')
 const walker = require('easy-file-walker')
 const expected = require('./data/commandOutput')
 
-describe('command', function () {
-  after(function (done) {
+module.exports = {
+  after: function (done) {
     return rimraf(path.resolve('./test/command'), function (err) {
       if (err != null) {
         console.log(err)
       }
       return done()
     })
-  })
+  },
 
-  it('should parse and copy', function (done) {
-    var sandbox
-    sandbox = {
-      model: 'Todo',
-      answers: {
-        persistence: 'REST',
-        auth: 'facebook'
-      },
-      package: {
-        name: 'myapp',
-        description: 'foo',
-        org: 'wearefractal',
-        author: 'fractal'
-      }
-    }
-
-    return boba({
-      src: './test/commandTemplates',
-      dest: './test/command',
-      currentKey: null,
-      opts: {
-        blacklist: ['.DS_Store'],
-        sandbox: sandbox,
-        key: {
-          language: 'coffee',
-          server: 'yes',
+  'command shuld': {
+    'should parse and copy': function (done) {
+      var sandbox
+      sandbox = {
+        model: 'Todo',
+        answers: {
+          persistence: 'REST',
           auth: 'facebook'
+        },
+        package: {
+          name: 'myapp',
+          description: 'foo',
+          org: 'wearefractal',
+          author: 'fractal'
         }
       }
-    }, function (err, res) {
-      if (err) console.log(err)
-      return walker.walk('./test/command').then(function (files) {
-        return files.sort().should.eql(expected.sort())
-      }).then(done.bind(null, null), done)
-    })
-  })
-})
+
+      return boba({
+        src: './test/commandTemplates',
+        dest: './test/command',
+        currentKey: null,
+        opts: {
+          blacklist: ['.DS_Store'],
+          sandbox: sandbox,
+          key: {
+            language: 'coffee',
+            server: 'yes',
+            auth: 'facebook'
+          }
+        }
+      }, function (err, res) {
+        if (err) console.log(err)
+        return walker.walk('./test/command').then(function (files) {
+          return files.sort().should.eql(expected.sort())
+        }).then(done.bind(null, null), done)
+      })
+    }
+  }
+}
--- test/interpolate.js
+++ test/interpolate.js
@@ -1,32 +1,35 @@
 require('should')
 const interpolate = require('../lib/interpolate')
 
-describe('interpolate', function () {
-  it('should return empty if passed falsey value', function () {
-    var result
-    result = interpolate('', {
-      name: 'boba'
-    })
-    result.should.equal('')
-    result = interpolate(null, {
-      name: 'boba'
-    })
-    return result.should.equal('')
-  })
+module.exports = {
 
-  it('should return parsed template', function () {
-    let template = 'hi <%= name %>'
-    let result = interpolate(template, {
-      name: 'boba'
-    })
-    result.should.equal('hi boba')
-    template = 'hi <%= name.last %>'
-    result = interpolate(template, {
-      name: {
-        first: 'boba',
-        last: 'fett'
-      }
-    })
-    return result.should.equal('hi fett')
-  })
-})
+  'interpolate': {
+    'should return empty if passed falsey value': function () {
+      var result
+      result = interpolate('', {
+        name: 'boba'
+      })
+      result.should.equal('')
+      result = interpolate(null, {
+        name: 'boba'
+      })
+      return result.should.equal('')
+    },
+
+    'should return parsed template': function () {
+      let template = 'hi <%= name %>'
+      let result = interpolate(template, {
+        name: 'boba'
+      })
+      result.should.equal('hi boba')
+      template = 'hi <%= name.last %>'
+      result = interpolate(template, {
+        name: {
+          first: 'boba',
+          last: 'fett'
+        }
+      })
+      return result.should.equal('hi fett')
+    }
+  }
+}
